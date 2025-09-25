5caedce0bdf646c1755940e0b53c2e4b64135c86
Karolis Simkus <daikasbaikalius@gmail.com>
2025-09-25 19:27:36 +0300

    Indexer

---
+++ SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    internal class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        // Custom indexer for this class.
+        public Person this[int index]
+        {
+        get => (Person) arPeople[index];
+                set => arPeople.Insert(index, value);
+        }
+        public int Count => arPeople.Count;
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleIndexer/PersonCollectionStringIndexer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    internal class PersonCollectionStringIndexer : IEnumerable
+    {
+        private Dictionary<string, Person> listPeople = new Dictionary<string, Person>();
+
+        // This indexer returns a person baed on a string index
+        public Person this[string name]
+        { 
+            get { return (Person)listPeople[name]; } 
+            set { listPeople[name] = value; } 
+        }
+        public void ClearPeople()
+        {
+            listPeople.Clear();
+        }
+        public int Count => listPeople.Count;
+        IEnumerator IEnumerable.GetEnumerator() => listPeople.GetEnumerator();
+    }
+}
