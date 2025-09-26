ec9d6814c04008ce84004ec511db9700042a7f04
Karolis Simkus <daikasbaikalius@gmail.com>
2025-09-26 18:06:34 +0300

    Delegates

---
+++ CarDelegate/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        // Is the car alive or dead?
+        private bool _carIsDead;
+        public Car() { }
+        public Car(string name,int maxSpeed, int currentSpeed) 
+        {
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = name;
+        }
+        // 1) Define a delegate type.
+        public delegate void CarEngineHandler(string msgForCaller);
+        // 2) Define a member variable of this delegate.
+        private CarEngineHandler _listOfHandlers;
+        // 3) Add registration function for the caller.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            _listOfHandlers += methodToCall;
+        }
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            _listOfHandlers -= methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+            // If this car is "dead," send dead message.
+            if (_carIsDead)
+            {
+                _listOfHandlers?.Invoke("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // Is this car "almost dead"?
+                if (10 == (MaxSpeed - CurrentSpeed))
+                {
+                    _listOfHandlers?.Invoke("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ SimpleDelegate/SimpleMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    public class SimpleMath
+    {
+        public static int Add(int a, int b) { return a + b; }
+        public static int Sub(int a, int b) {return a - b; }
+        public static int Square(int a) => a * a;
+    }
+}
